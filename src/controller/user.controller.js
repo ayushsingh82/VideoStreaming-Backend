0a7ccd84e1c38e47aedf8a4f5c68128a52e85bb4
Ayush Singh <ayushsinghmi711@gmail.com>
2023-12-18 14:52:29 +0530

    user created

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,6 +1,7 @@
 import {asyncHandler} from '../utils/asyncHandler.js';
 import {ApiError} from "../utils/ApiError.js"
 import { User } from '../models/user.model.js';
+import { uploadOnCloudinary } from '../utils/cloudinary.js';
 
 
 const registerUser=asyncHandler(async(req,res)=>{
@@ -36,6 +37,22 @@ const registerUser=asyncHandler(async(req,res)=>{
     if(!avatarLocalPath){
       throw new ApiError(400,"Avatar file is required")
     }
+
+    const avatar=await uploadOnCloudinary(avatarLocalPath);
+    const coverImage=uploadOnCloudinary(coverImageLocalPath)
+
+    if(!avatar){
+      throw new ApiError(400,"Avatar file is required")
+    }
+
+    const user=await User.create({
+      fullName,
+      avatar:avatar.url,
+      coverImage:coverImage?.url || "",
+      email,
+      password,
+      username:username.toLowerCase()
+    })
 })
 
 export {registerUser}
