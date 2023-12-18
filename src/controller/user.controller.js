7b57941e0894c7238f0912db271f8872e77c1f30
Ayush Singh <ayushsinghmi711@gmail.com>
2023-12-18 14:42:16 +0530

    exist user checked

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,5 +1,7 @@
 import {asyncHandler} from '../utils/asyncHandler.js';
 import {ApiError} from "../utils/ApiError.js"
+import { User } from '../models/user.model.js';
+
 
 const registerUser=asyncHandler(async(req,res)=>{
     // res.status(200).json({
@@ -18,6 +20,21 @@ const registerUser=asyncHandler(async(req,res)=>{
      field?.trim()==="")
     ){
       throw new ApiError(400,"All fields are required")
+    }
+
+    const existedUser=User.findOne({
+      $or:[{username},{email}]
+    })
+
+    if(existedUser){
+      throw new ApiError(409,"User already exist")
+    }
+
+    const avatarLocalPath=req.files?.avatar[0]?.path;
+    const coverImageLocalPath=req.files?.coverImage[0]?.path;
+
+    if(!avatarLocalPath){
+      throw new ApiError(400,"Avatar file is required")
     }
 })
 
